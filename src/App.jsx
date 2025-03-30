b06c744ec217f61970c826cd1ed94fc60a1fc93a
Abhiseck Bhattacharya <abhiseck@outlook.com>
2025-03-30 21:14:43 +0530

    Add react-router for navigation and update App component structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
-function App() {
+import { createBrowserRouter, Link, RouterProvider } from "react-router";
+
+const HomePage = () => {
   const name = import.meta.env.VITE_NAME || "Simple Testing Site";
   const link =
     import.meta.env.VITE_LINK ||
@@ -12,8 +14,58 @@ function App() {
           <a href={link}>Github</a>
         </h2>
       </div>
+      <ul>
+        <li>
+          <Link to={"/"}>Home</Link>
+        </li>
+        <li>
+          <Link to={"/about"}>About</Link>
+        </li>
+        <li>
+          <Link to={"/contact"}>Contact</Link>
+        </li>
+      </ul>
     </div>
   );
+};
+
+const SimplePage = ({ pageName }) => {
+  return (
+    <div>
+      <h1>Simple Testing Site</h1>
+      <ul>
+        <li>
+          <Link to={"/"}>Home</Link>
+        </li>
+        <li>
+          <Link to={"/about"}>About</Link>
+        </li>
+        <li>
+          <Link to={"/contact"}>Contact</Link>
+        </li>
+      </ul>
+      <h1>/{pageName}</h1>
+    </div>
+  );
+};
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+  },
+  {
+    path: "/about",
+    element: <SimplePage pageName={"about"} />,
+  },
+  {
+    path: "/contact",
+    element: <SimplePage pageName={"contact"} />,
+  },
+]);
+
+function App() {
+  return <RouterProvider router={router} />;
 }
 
 export default App;
